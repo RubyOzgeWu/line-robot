d95b2f2f3abab078787187caa00a0d5872e9cfe9
RubyOzgeWu <rubyozgewu@gmail.com>
2022-06-04 12:12:56 +0800

    第一次上傳git

---
+++ index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import linebot from 'linebot'
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動')
+})
