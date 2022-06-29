65608a4b6fffdc5e15392561f86c6b57b2996b59
Ruby Oz <rubyozgewu@gmail.com>
2022-06-29 19:51:16 +0800

    test

--- template.js
+++ template.js
@@ -1,3 +1,31 @@
+// export default {
+//   type: 'bubble',
+//   body: {
+//     type: 'box',
+//     layout: 'vertical',
+//     contents: [
+//       {
+//         type: 'text',
+//         text: 'Brown Cafe',
+//         weight: 'bold',
+//         size: 'xl'
+//       },
+//       {
+//         type: 'text',
+//         text: 'dishes'
+//       },
+//       {
+//         type: 'text',
+//         text: 'address'
+//       },
+//       {
+//         type: 'text',
+//         text: 'tel'
+//       }
+//     ]
+//   }
+// }
+
 export default {
   type: 'bubble',
   body: {
@@ -21,6 +49,15 @@ export default {
       {
         type: 'text',
         text: 'tel'
+      },
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "location",
+          "label": "Google map"
+        }
       }
     ]
   }
